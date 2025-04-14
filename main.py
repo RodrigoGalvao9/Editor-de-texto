0c81e40405ccec3f2421a733723983622816efbc
Rodrigo Galvão <rodrigogalvao136@gmail.com>
2025-04-13 22:44:57 -0300

    FIX: correção na importação do modulo externo da biblioteca spellchecker, além de desabilitar o cmd no .exe

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
 from PySide6.QtWidgets import QApplication
 from App.paginaInicial import NotePad
 import sys
 
 if __name__ == "__main__":
+    dev_mode = os.getenv("DEV_MODE", "false").lower() == "true"
+
+    if dev_mode:
+        print("Modo de desenvolvimento ativado. Logs serão exibidos no terminal.")
+
     app = QApplication(sys.argv)
     main_window = NotePad()
     main_window.show()
